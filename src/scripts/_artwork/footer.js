d250e0d5a3f99370ffb208cd796436c45bf054f2
Kelly Milligan <kelly@milligan.net.nz>
2016-10-17 17:02:22 +0200

    Playing with footer setup

--- src/scripts/_artwork/footer.js
+++ src/scripts/_artwork/footer.js
@@ -2,6 +2,8 @@ import { _, $, BaseObject } from '../common';
 
 import Artwork from '../_artwork/artwork';
 
+import drawCircle from '../utils/canvas/draw_circle';
+
 export default Object.assign( Object.create( BaseObject ), {
 
 
@@ -17,13 +19,14 @@ export default Object.assign( Object.create( BaseObject ), {
 
     draw: function () {
 
-        console.log( this.width );
-
-        const FOOTER_HEIGHT = 200;
+        const FOOTER_HEIGHT = 160;
+        const STAMP_RADIUS = 40;
 
         this.ctx.canvas.width = this.width;
         this.ctx.canvas.height = FOOTER_HEIGHT;
 
+        // Background
+        this.ctx.save();
         this.ctx.fillStyle = "#fff";
         this.ctx.fillRect(
             0,
@@ -31,11 +34,21 @@ export default Object.assign( Object.create( BaseObject ), {
             this.ctx.canvas.width,
             FOOTER_HEIGHT
         );
+        this.ctx.restore();
 
+        // Stamp
+        this.ctx.save();
+        this.ctx.fillStyle = 'rgb(37, 26, 48)';
+        drawCircle( this.ctx, STAMP_RADIUS, FOOTER_HEIGHT - STAMP_RADIUS, STAMP_RADIUS );
+        this.ctx.fill();
+        this.ctx.restore();
+
+        // Text
+        this.ctx.save();
         this.ctx.fillStyle = '#000';
-        this.ctx.font = "60px 'Roboto Mono'";
-        this.ctx.textBaseline = "middle";
-        this.ctx.fillText("Footer Text...", 0, FOOTER_HEIGHT * 0.5);
+        this.ctx.font = '60px bold "Roboto Mono"';
+        this.ctx.fillText("Footer Text...", STAMP_RADIUS * 2 + STAMP_RADIUS, FOOTER_HEIGHT - STAMP_RADIUS * 0.5 );
+        this.ctx.restore();
     }
 
 });
