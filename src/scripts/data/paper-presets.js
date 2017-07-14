160da680770fc8db731788591849d240c2c46f05
Kelly Milligan <kelly@milligan.net.nz>
2017-07-14 10:53:22 +0100

    Add paper size data object

---
+++ src/scripts/data/paper-presets.js
@@ -0,0 +1,229 @@
+export default {
+
+    // A series sizes
+    // Source: papersizes.io/a
+    'A': [
+        {
+            id: '4A0',
+            size: {
+                metric: {
+                    width: 1682,
+                    height: 2378
+                },
+                imperial: {
+                    width: 66.2,
+                    height: 93.6
+                },
+                screen: {
+                    width: 20184,
+                    height: 28536
+                }
+            }
+        },
+        {
+            id: '2A0',
+            size: {
+                metric: {
+                    width: 1189,
+                    height: 1682
+                },
+                imperial: {
+                    width: 46.8,
+                    height: 66.2
+                },
+                screen: {
+                    width: 20184,
+                    height: 14268
+                }
+            }
+        },
+        {
+            id: 'A0',
+            size: {
+                metric: {
+                    width: 841,
+                    height: 1189
+                },
+                imperial: {
+                    width: 33.1,
+                    height: 46.8
+                },
+                screen: {
+                    width: 10092,
+                    height: 14268
+                }
+            }
+        },
+        {
+            id: 'A1',
+            size: {
+                metric: {
+                    width: 594,
+                    height: 841
+                },
+                imperial: {
+                    width: 23.4,
+                    height: 33.1
+                },
+                screen: {
+                    width: 7128,
+                    height: 10092
+                }
+            }
+        },
+        {
+            id: 'A2',
+            size: {
+                metric: {
+                    width: 420,
+                    height: 594
+                },
+                imperial: {
+                    width: 16.5,
+                    height: 23.4
+                },
+                screen: {
+                    width: 5040,
+                    height: 7128
+                }
+            }
+        },
+        {
+            id: 'A3',
+            size: {
+                metric: {
+                    width: 297,
+                    height: 420
+                },
+                imperial: {
+                    width: 11.7,
+                    height: 16.5
+                },
+                screen: {
+                    width: 3564,
+                    height: 5040
+                }
+            }
+        },
+        {
+            id: 'A4',
+            size: {
+                metric: {
+                    width: 210,
+                    height: 297
+                },
+                imperial: {
+                    width: 8.3,
+                    height: 11.7
+                },
+                screen: {
+                    width: 2520,
+                    height: 3564
+                }
+            }
+        },
+        {
+            id: 'A5',
+            size: {
+                metric: {
+                    width: 148,
+                    height: 210
+                },
+                imperial: {
+                    width: 5.8,
+                    height: 8.3
+                },
+                screen: {
+                    width: 1776,
+                    height: 2520
+                }
+            }
+        },
+        {
+            id: 'A6',
+            size: {
+                metric: {
+                    width: 105,
+                    height: 148
+                },
+                imperial: {
+                    width: 4.1,
+                    height: 5.8
+                },
+                screen: {
+                    width: 1260,
+                    height: 1776
+                }
+            }
+        },
+        {
+            id: 'A7',
+            size: {
+                metric: {
+                    width: 74,
+                    height: 105
+                },
+                imperial: {
+                    width: 2.9,
+                    height: 4.1
+                },
+                screen: {
+                    width: 888,
+                    height: 1260
+                }
+            }
+        },
+        {
+            id: 'A8',
+            size: {
+                metric: {
+                    width: 52,
+                    height: 74
+                },
+                imperial: {
+                    width: 2,
+                    height: 2.9
+                },
+                screen: {
+                    width: 624,
+                    height: 888
+                }
+            }
+        },
+        {
+            id: 'A9',
+            size: {
+                metric: {
+                    width: 37,
+                    height: 52
+                },
+                imperial: {
+                    width: 1.5,
+                    height: 2
+                },
+                screen: {
+                    width: 444,
+                    height: 624
+                }
+            }
+        },
+        {
+            id: 'A10',
+            size: {
+                metric: {
+                    width: 26,
+                    height: 37
+                },
+                imperial: {
+                    width: 1,
+                    height: 1.5
+                },
+                screen: {
+                    width: 312,
+                    height: 444
+                }
+            }
+        }
+    ]
+
+};
